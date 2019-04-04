bf44e5bd3a3b6ba6228339dc826bf6da3cab162d
zhang-xiao-miao <1286874200@qq.com>
2019-04-04 18:29:59 -0400

    reconstruct MerchantList Component

--- src/components/MerchantList/index.js
+++ src/components/MerchantList/index.js
@@ -1,24 +1,69 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import { Link } from '@reach/router';
 import { PropTypes } from 'prop-types';
 import MerchantItem from './Item';
+import LoadMore from '../../components/LoadMore';
+import * as helpers from '../../utils/helpers';
+import lodash from 'lodash';
+import * as api from '../../utils/api';
 
 import './style.scss';
 
-const MerchantList = ({ merchants }) => (
-  <ul className="merchant-list">
-    {merchants.map(merchant => (
-      <li key={merchant.id}>
-        <Link to={`/merchant/${merchant.id}`} className="to-merchant">
-          <MerchantItem merchant={merchant} />
-        </Link>
-      </li>
-    ))}
-  </ul>
-);
+export default class MerchantList extends Component {
+  state = {
+    merchants: [],
+    meta: {
+      page: 1,
+      size: 10,
+      isLoading: false,
+      more: true
+    }
+  };
+
+  render() {
+    const merchants = this.state.merchants;
+    return (
+      <Fragment>
+        <ul className="merchant-list">
+          {merchants.map(merchant => (
+            <li key={merchant.id}>
+              <Link to={`/merchant/${merchant.id}`} className="to-merchant">
+                <MerchantItem merchant={merchant} />
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <LoadMore />
+      </Fragment>
+    );
+  }
+
+  fetchMerchants = async () => {
+    let query = {
+      page: this.state.meta.page,
+      size: this.state.meta.size,
+      keywords: this.props.keywords,
+      category: this.props.category
+    };
+    query = helpers.clean(query);
+    const { meta, data } = await api.fetchMerchants(query).catch(e => {
+      console.log(e, query);
+      window.alert('Fetch merchant list failed!');
+    });
+    const merchants = lodash.cloneDeep(this.state.merchants);
+    this.setState({
+      meta: {
+        isLoading: false,
+        page: meta.more ? query.page + 1 : query.page,
+        more: meta.more,
+        size: query.size
+      },
+      merchants: merchants.concat(data)
+    });
+  };
+}
 
 MerchantList.propTypes = {
-  merchants: PropTypes.array.isRequired
+  keywords: PropTypes.string,
+  category: PropTypes.string
 };
-
-export default MerchantList;
